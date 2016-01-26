62ba40e07e0f84ed9b7b2c79a6d96ece44b0d05d
Nathan <ottoetc@gmail.com>
2016-01-26 10:08:12 -0800

    fix right column

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,40 @@
+//Address Book
+
 function Contact(firstName, lastName) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.addresses = [];
 }
+
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
 function Address(addressType, street, city, state) {
   this.addressType = addressType;
   this.street = street;
   this.city = city;
   this.state = state;
 }
+
 Address.prototype.fullAddress = function() {
   return this.addressType + ": " + this.street + ", " + this.city + ", " + this.state;
 }
 
+function resetFields() {
+  $("input#new-first-name").val("");
+  $("input#new-last-name").val("");
+  $("select.new-address-type").val("");
+  $("input.new-street").val("");
+  $("input.new-city").val("");
+  $("input.new-state").val("");
+}
 
 $(document).ready(function() {
   $("#add-address").click(function() {
     $("#new-addresses").append('<div class="new-address">' + '<div class="form-group">' + '<label for="new-address-type">Address type</label>' + '<select class="form-control new-address-type">' + '<option></option>' + '<option>Home</option>' + '<option>Work</option>' + '<option>School</option>' + '<option>Other</option>' + '</select>' + '</div>' + '<div class="form-group">' + '<label for="new-street">Street</label>' + '<input type="text" class="form-control new-street">' + '</div>' + '<div class="form-group">' + '<label for="new-city">City</label>' + '<input type="text" class="form-control new-city">' + '</div>' + '<div class="form-group">' + '<label for="new-state">State</label>' + '<input type="text" class="form-control new-state">' + '</div>' + '</div>');
   });
+
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
@@ -37,15 +51,7 @@ $(document).ready(function() {
     newContact.addresses.push(newAddress);
   });
 
-
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("select.new-address-type").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
@@ -57,5 +63,8 @@ $(document).ready(function() {
         $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
       });
     });
+
+    resetFields();
+
   });
 });
